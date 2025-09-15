b912aaeafd2d4767c05ab9a53b05375cdc4b4641
Nox <115188962+sleepwalker105@users.noreply.github.com>
2025-09-15 17:36:54 +0200

    Delete Nowhere directory

--- Nowhere/js/config.js
+++
@@ -1,6 +0,0 @@
-// Конфигурация (НЕ коммитьте токен в репозиторий!)
-const CONFIG = {
-    github: {
-        token: process.env.GITHUB_TOKEN || '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'
-    }
-};
